883300bf46b39ae45b247cd821930ff6bc2c5064
Guilherme Azevedo <guiazevedo.deb@gmail.com>
2023-05-17 01:08:35 -0300

    refactor: using prettier-plugin-tailwindcss code style.

--- App.tsx
+++ App.tsx
@@ -3,8 +3,8 @@ import { Text, View } from 'react-native'
 
 export default function App() {
   return (
-    <View className="bg-zinc-950 flex-1 items-center justify-center">
-      <Text className="text-zinc-50 font-bold text-5xl">Hello, world!</Text>
+    <View className="flex-1 items-center justify-center bg-zinc-950">
+      <Text className="text-5xl font-bold text-zinc-50">Hello, world!</Text>
       <StatusBar style="auto" />
     </View>
   )
